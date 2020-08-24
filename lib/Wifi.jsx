92ec03661f5daa95b9e1e579e55e819ae9999d2f
Michael Utz <michael@theutz.com>
2020-08-24 09:53:34 +0300

    Add WiFi emoji

--- lib/Wifi.jsx
+++ lib/Wifi.jsx
@@ -2,8 +2,8 @@ const render = ({ output }) => {
   if (typeof output === "undefined") return null;
   const status = output.status;
   const ssid = output.ssid;
-  if (status === "inactive") return <div>􀙈</div>;
-  return <div>􀙇 {output.ssid}</div>;
+  if (status === "inactive") return <div>🏮️</div>;
+  return <div>📡 &nbsp;&nbsp;{output.ssid}</div>;
 };
 
 export default render;
